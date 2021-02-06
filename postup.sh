d9b72a6c3e11eb82a9562c36c072c78f87bb3c7f
ThreeCO <42574949+ThreeCO@users.noreply.github.com>
2021-02-06 22:15:51 +0100

    Update postup.sh

--- postup.sh
+++ postup.sh
@@ -1,6 +1,7 @@
-WIREGUARD_INTERFACE=wg0
-WIREGUARD_LAN=10.0.0.0/24
-MASQUERADE_INTERFACE=eth0
+WIREGUARD_INTERFACE="wg0"
+WIREGUARD_LAN="10.0.0.0/24"
+MASQUERADE_INTERFACE="eth0"
+PORT=5280
 
 # Add MASQUERADE-RULE
 sudo iptables -t nat -I POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE -s $WIREGUARD_LAN
@@ -14,11 +15,11 @@ sudo iptables -A FORWARD -j $CHAIN_NAME
 sudo iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # jump to TCP/UPD Blacklists
-sudo iptables -A $CHAIN_NAME -s 10.0.0.0/24 -i $WIREGUARD_INTERFACE -p tcp -j FW_WG_TCP
-sudo iptables -A $CHAIN_NAME -s 10.0.0.0/24 -i $WIREGUARD_INTERFACE -p udp -j FW_WG_UDP
+sudo iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -p tcp -j FW_WG_TCP
+sudo iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -p udp -j FW_WG_UDP
 
 # Accept traffic from any Wireguard IP address connected to the Wireguard server
-sudo iptables -A $CHAIN_NAME -s 10.0.0.0/24 -i $WIREGUARD_INTERFACE -j ACCEPT
+sudo iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
 
 # deny with icmp-message
 sudo iptables -A $CHAIN_NAME -i $WIREGUARD_INTERFACE -j REJECT --reject-with icmp-host-unreachable
@@ -30,5 +31,5 @@ sudo iptables -A $CHAIN_NAME -i $WIREGUARD_INTERFACE -j DROP
 sudo iptables -A $CHAIN_NAME -j RETURN
 
 # ADD WG-Port to INPUT chain
-sudo iptables -A UDP -p udp --dport 5280 -j ACCEPT
+sudo iptables -A UDP -p udp --dport $PORT -j ACCEPT
 
