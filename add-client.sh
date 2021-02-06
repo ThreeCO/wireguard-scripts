7395b6e6b9cd292004238da24ef6aba680a70e0e
ThreeCO <42574949+ThreeCO@users.noreply.github.com>
2021-02-06 13:37:46 +0100

    Update add-client.sh
    Added config-variables

--- add-client.sh
+++ add-client.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+srv_host = "172.0.0.1" # external IP of Wireguard
+srv_port = "5280" # external Port of Wireguard
+cl_dns = "127.0.0.1" # which DNS-Server shall be used?
+
+
 if [ $# -eq 0 ]
 then
 	echo "must pass a client name as an arg: add-client.sh new-client"
@@ -10,12 +15,13 @@ elsets/
         cat clients/$1/$1.priv | wg pubkey > clients/$1/$1.pub
 	wg genpsk > clients/$1/$1.pre
 	
-	key=$(cat clients/$1/$1.priv) 
-	pub=$(cat clients/$1/$1.pub)
-	pre=$(cat clients/$1/$1.pre)
-	ip="10.0.0."$(expr $(cat clients/last-ip.txt | tr "." " " | awk '{print $4}') + 1)
+	cl_priv=$(cat clients/$1/$1.priv) 
+	cl_pub=$(cat clients/$1/$1.pub)
+	cl_pre=$(cat clients/$1/$1.pre)
+	cl_ip="10.0.0."$(expr $(cat clients/last-ip.txt | tr "." " " | awk '{print $4}') + 1)
 	FQDN=$(hostname -f)
-        SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
+        srv_pub=$(cat /etc/wireguard/server_public_key)
+	
         cat clients/wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
 	echo $ip > clients/last-ip.txt
 	#cp SETUP.txt clients/$1/SETUP.txt
