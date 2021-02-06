d9ae827cbb3a5c2f6c29be8732df0f6001256559
ThreeCO <42574949+ThreeCO@users.noreply.github.com>
2021-02-06 20:18:14 +0100

    Update add-client.sh
    Cleaned script

--- add-client.sh
+++ add-client.sh
@@ -24,7 +24,6 @@ else
 	cl_pre  = $(cat clients/$1/$1.pre)
 	cl_ip   = $cl_ip""$(expr $(cat clients/last-ip.txt | tr "." " " | awk '{print $4}') + 1)
 	cl_sts  = "0.0.0.0/0"
-	FQDN    = $(hostname -f)
         srv_pub = $(cat /etc/wireguard/server_public_key)
 	
 	if [ $2 -eq "true"]
@@ -34,8 +33,6 @@ else
 	
         cat clients/wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$cl_ip"'/' | sed -e 's|:CLIENT_KEY:|'"$cl_priv"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$srv_pub"'|' | sed -e 's|:PRESHARED_KEY:|'"$pub_pre"'|' | sed -e 's|:ALLOWED_IPS:|'"$cl_allowed"'|' | sed -e 's|:SERVER_ADDRESS:|'"$srv_host"'|' | sed -e 's|:SERVER_PORT:|'"$srv_port"'|' > clients/$1/wg0.conf
 	echo $ip > clients/last-ip.txt
-	#cp SETUP.txt clients/$1/SETUP.txt
-	#tar czvf clients/$1.tar.gz clients/$1
 	echo "Created config!"
 	
 	echo "Adding peer to wg-conf"
@@ -43,11 +40,7 @@ else
 	cat clients/wg0-server.example.conf | # to befinished  >> wg0.conf
 	
 	echo "Adding peer to hosts file"
-	echo $ip" "$1 | sudo tee -a /etc/hosts
-	
-	# restart wg0
-	# sudo wg-quick down $wg_name
-	# sudo wg-quick up $wg_name
+	echo $ip" "$1 | sudo tee -a /etc/host
 
         # resyncing wireguard 
         sudo wg syncconf $wg_name <(wg-quick strip $wg_name)
